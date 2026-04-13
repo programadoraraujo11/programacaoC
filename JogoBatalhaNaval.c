bfda5a940123195ed3c2a0d3b14a11c41a73906d
programadoraraujo11 <leonardoaraujopm93@gmail.com>
2026-04-13 17:22:30 +0000

    Jogo Batalha Naval nível novato

---
+++ JogoBatalhaNaval.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int main () {
+
+    int tabuleiro[10][10];
+    int navio[3] ={3, 3, 3};
+
+    int i, j;
+
+    // Preencher o tabuleiro com 0
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            tabuleiro[i][j] = 0;        }
+        
+    }
+
+    // Navio Horizontal (linha 2, começando na coluna 4)
+    for (i = 0; i < 3; i++)
+    {
+        tabuleiro[2][4 + i] = navio[i];
+
+    }
+    
+    // Navio vertical (coluna 7, começando na linha 5)
+    for (i = 0; i < 3; i++)
+    {
+        tabuleiro[5 + i][7] = navio[i];
+    }
+    
+    // Mostrar tabuleiro
+    printf("TABULEIRO BATALHA NAVAL\n");
+
+    printf("   A B C D E F G H I J \n");
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("%2d ", i + 1);
+
+        for (j = 0; j < 10; j++)
+        {
+            printf("%d ", tabuleiro[i][j]);
+
+        }
+        
+        printf("\n");
+    }
+    
+
+    return 0;
+}
